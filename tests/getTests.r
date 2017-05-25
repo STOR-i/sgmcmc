bd08a719a6648ac26abd322449f84d583f672068
Jack Baker <j.baker1@lancaster.ac.uk>
2017-05-25 11:21:10 +0100

    Added .Rbuildignore

--- tests/getTests.r
+++
@@ -1,9 +0,0 @@
-library('RUnit')
- 
-testSuite = defineTestSuite("checkConvergence",
-                              dirs = file.path("./"),
-                              testFileRegexp = "^runit.3dgauss.r")
- 
-testResults <- runTestSuite(testSuite)
- 
-printTextProtocol(testResults)
